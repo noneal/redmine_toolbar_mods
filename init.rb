8b69a069c41df0ffe4cc8d41730c6c37e00762b1
noah <noah@redminetest.nlgames.com>
2013-09-19 14:39:13 -0700

    Fixed apostrophes

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ require_dependency 'ewwiki_hook'
 
 Redmine::Plugin.register :redmine_toolbar_mods do
   name "Toolbar modifications"
-  author 'Tavish Armstrong & Noah O'Neal'
+  author 'Tavish Armstrong & Noah O Neal'
   description 'Added colour buttons to toolbar'
   version '0.0.2'
 end
