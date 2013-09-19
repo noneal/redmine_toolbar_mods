027926dffddd57391d21094b8ffaf185446fed38
noah <noah@redminetest.nlgames.com>
2013-09-19 14:37:46 -0700

    Updated README and init.rb

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ require 'redmine'
 require_dependency 'ewwiki_hook'
 
 Redmine::Plugin.register :redmine_toolbar_mods do
-  name "Tavish's awesome toolbar modifications"
-  author 'Tavish Armstrong'
-  description 'Some toolbar modifications'
-  version '0.0.1'
+  name "Toolbar modifications"
+  author 'Tavish Armstrong & Noah O'Neal'
+  description 'Added colour buttons to toolbar'
+  version '0.0.2'
 end
